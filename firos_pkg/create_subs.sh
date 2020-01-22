105d944d2993f4bb4972f86ca121b94cbf4b5561
Henrique Poleselo <hpoleselo@gmail.com>
2020-01-21 22:21:37 -0300

    Deleted the folder from Felipe since the creation of the subscription is made by the new shell added and not y the previous Python scripts.

---
+++ firos_pkg/create_subs.sh
@@ -0,0 +1,35 @@
+curl --request POST \
+  --url 'http://150.162.6.64:1026/v2/subscriptions/' \
+  --header 'content-type: application/json' \
+  --data '{
+  "description": "Pick-and-place robot changes",
+  "subject": {
+    "entities": [{"idPattern": ".*", "type": "pickandplace"}],
+    "condition": { 
+      "attrs": [ ]
+    }
+  },
+  "notification": {
+    "http": {
+      "url": "http://quantumleap:8668/v2/notify"
+    }
+  }
+}' 
+
+curl --request POST \
+  --url 'http://150.162.6.64:1026/v2/subscriptions/' \
+  --header 'content-type: application/json' \
+  --data '{
+  "description": "3D Printer changes",
+  "subject": {
+    "entities": [{"idPattern": ".*", "type": "3dprinter"}],
+    "condition": { 
+      "attrs": [ ]
+    }
+  },
+  "notification": {
+    "http": {
+      "url": "http://quantumleap:8668/v2/notify"
+    }
+  }
+}' 
--- firos_pkg/src_flpe/create_subs.py
+++
@@ -1,9 +0,0 @@
-from fiware import OrionClient
-
-head = {"Content-Type": "application/json"}
-host = "150.162.6.64"
-port = 1026
-
-client = OrionClient(host, port)
-print("Ois")
-client.subscription("pickandplace")
